3b7aca7469fa8c6264ce2f24b51b2e1c76bdfc45
w3i3538 <wei5566@gmail.com>
2023-07-29 21:38:11 +0800

    feat: login feature

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const passport = require('passport')
+const usePassport = require('./config/passport')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
@@ -10,8 +13,17 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.get('/', (req, res) => {
     return Todo.findAll({
@@ -33,9 +45,10 @@ app.get('/users/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-    res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
     res.render('register')
