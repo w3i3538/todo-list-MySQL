cdd797fe8b1b8f80777884c09ddd23983f3abf7d
w3i3538 <wei5566@gmail.com>
2023-07-31 17:53:41 +0800

    fix: wrong path

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-const db = require('./models')
+const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
 
